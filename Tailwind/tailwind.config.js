39ef8d5dd6a7d99da83e2e820f82b408d3e9b02b
imagham <151702624+imagham@users.noreply.github.com>
2024-01-11 13:28:51 +0700

    Telwind

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'salsa':'salsa'
+      }
+    },
+  },
+  plugins: [],
+}
+
