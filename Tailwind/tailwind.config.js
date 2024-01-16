03d040efa60011a0bfeb2db4a8a9f0f21c84db47
imagham <151702624+imagham@users.noreply.github.com>
2024-01-16 14:13:38 +0700

    update

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'salsa':'salsa'
+        'salsa':'salsa',
+      },
+      fontSize:{
+        'fadgham':'30px',
+      },
+      backgroundImage :{
+        kucing:'url("../public/image/kucing.jpeg")',
+        sawah:'url("../public/image/sawah.jpg")'
       }
     },
   },
