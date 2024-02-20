3f321161c2f31df098a63887dcf266f6fe03ac45
imagham <151702624+imagham@users.noreply.github.com>
2024-02-20 13:07:05 +0700

    update responsive dan dark mode

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,25 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'salsa':'salsa',
+        salsa: "salsa",
       },
-      fontSize:{
-        'fadgham':'30px',
-         '22':'22px'
+      fontSize: {
+        fadgham: "30px",
+        22: "22px",
       },
-      backgroundImage :{
-        kucing:'url("../public/image/kucing.jpeg")',
-        sawah:'url("../public/image/sawah.jpg")',
-        ustadz:'url("../public/image/ustadz.jpg")',
-        biru :'url("../public/image/biru.jpeg")',
-        roti :'url("../public/image/roti.jpg")',
-
-      }   
+      backgroundImage: {
+        kucing: 'url("../public/image/kucing.jpeg")',
+        sawah: 'url("../public/image/sawah.jpg")',
+        ustadz: 'url("../public/image/ustadz.jpg")',
+        biru: 'url("../public/image/biru.jpeg")',
+        roti: 'url("../public/image/roti.jpg")',
+      },
+      screens: {
+        "4k": "1500px",
+      },
     },
   },
   plugins: [],
-}
-
+};
